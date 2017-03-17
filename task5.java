a0e43a296d2a5274516de97f14f7c8426755dda0
Maxim--- <boundlessroom@gmail.com--->
2017-03-17 14:01:23 +0300

    Добавлен файл task5.java

---
+++ task5.java
@@ -0,0 +1,14 @@
+/**
+ * Created by student8 on 15.02.2017.
+ */
+public class task5 {
+    public static void main(String[] args) {
+
+    int a= 145;
+        int b=a%10;
+        int c=(a/10)%10;
+        int d= (a/100)%10;
+        System.out.println("Сумма цифр числа "+a+" равна "+(b+c+d));
+
+    }
+}
