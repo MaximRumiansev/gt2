d6b86be4e6d8ba90933dfd5f211f0f57416cccce
Maxim--- <boundlessroom@gmail.com--->
2017-03-17 14:02:09 +0300

    Добавлен файл t6.java

---
+++ t6.java
@@ -0,0 +1,12 @@
+/**
+ * Created by student8 on 15.02.2017.
+ */
+import java.util.Scanner;
+public class t6 {
+    public static void main (String[] args){
+        Scanner ew = new Scanner(System.in);
+            System.out.printf("Вес на луне равен %.2f килограмм", ew.nextInt()*0.17);
+
+
+    }
+}
